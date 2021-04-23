be2e976ce9da5cab124794c98c8811cbc54eab0b
KandasamyParthasarathy <kparthas@stevens.edu>
2021-04-23 13:17:20 -0400

    shelter route to individual shelter handlebars

--- routes/shelter.js
+++ routes/shelter.js
@@ -1,2 +1,19 @@
 const express = require("express");
 const router = express.Router();
+
+router.get("/:id", async (req, res) => {
+	if (!req.params.id) {
+		//res.status(404).render("error", {title: "404 Error", error: "No id supplied.", number: "404"});
+		return;
+	}
+
+	try {
+
+		res.status(200).render("pets/individual_shelter");
+	} catch (e) {
+		//res.status(404).render("error", { title: "404 Error", error: "No pet was found.", number: 404 });
+	} 
+});
+
+
+module.exports = router;
