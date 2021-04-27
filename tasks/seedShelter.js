2b47adc2ee8823a3b7c09285e8f52dba5221f6a6
Preet Dabhi <preetdabhi14@gmail.com>
2021-04-27 17:25:17 -0400

    Handlebar update, allshelter card-view & added search box in main

--- tasks/seedShelter.js
+++ tasks/seedShelter.js
@@ -102,11 +102,99 @@ async function main() {
         }
 
     );
+    const pamanimalrescue = await shelter.create("Pam beesly Adoption Company",
+        "pambeeslyadoption@gmail.com",
+        {
+            streetAddress1: "Calvert Street",
+            streetAddress2: "",
+            city: " Van Nuys",
+            stateCode: "PA",
+            zipCode: '13831'
+        },
+        'This is the bio for the shelter owned by pam.',
+        '201 344 5447',
+        "pambeeslyanimalrescue.com",
+        ["facebook.com/pambeeslyanimalrescue", "twitter.com/pambeeslyanimalrescue"],
+        ["6063d472f5a17f32a5cbdc24", "6064a5d02f56e4da24413ab3"],
+        ["6064a5f081004bcfbc33d6c5"],
+        {
+            _id: ObjectId("6063d5103833261e97e0920b"),
+            reviewDate: Date("2021-02-2T06:01:17.171Z"),
+            rating: 5,
+            reviewBody: "They were very nice and helpful when I was looking for a new pet to adopt!",
+            reviewer: ObjectId("6063d0e4be26b8a5128c59e1")
+        },
+        'http://pambeeslyanimalrescue.jpg',
+        {
+            _id: ObjectId("507f1f77bcf86cd799439011"),
+            feedback: "This website is user friendly. Posting adoptable pets through this website was easy.",
+            date: Date("2020-05-02T06:01:17.171Z"),
+            rating: 5
+        }
 
-    console.log(michealanimalrescue)
-    console.log(dwightanimalrescue)
-    console.log(jimanimalrescue)
+    );
+    const kellyanimalrescue = await shelter.create("kelly kapoor Adoption Company",
+        "kellykapooradoption@gmail.com",
+        {
+            streetAddress1: "Calvert Street",
+            streetAddress2: "",
+            city: " Van Nuys",
+            stateCode: "PA",
+            zipCode: '13831'
+        },
+        'This is the bio for the shelter owned by kelly.',
+        '201 344 5447',
+        "kellykapooranimalrescue.com",
+        ["facebook.com/kellykapooranimalrescue", "twitter.com/kellykapooranimalrescue"],
+        ["6063d472f5a17f32a5cbdc24", "6064a5d02f56e4da24413ab3"],
+        ["6064a5f081004bcfbc33d6c5"],
+        {
+            _id: ObjectId("6063d5103833261e97e0920b"),
+            reviewDate: Date("2021-03-21T06:01:17.171Z"),
+            rating: 5,
+            reviewBody: "They were very nice and helpful when I was looking for a new pet to adopt!",
+            reviewer: ObjectId("6063d0e4be26b8a5128c59e1")
+        },
+        'http://kellykapooranimalrescue.jpg',
+        {
+            _id: ObjectId("507f1f77bcf86cd799439011"),
+            feedback: "This website is user friendly. Posting adoptable pets through this website was easy.",
+            date: Date("2020-05-02T06:01:17.171Z"),
+            rating: 5
+        }
 
+    );
+    const ryananimalrescue = await shelter.create("ryan howard Adoption Company",
+        "ryanhowardadoption@gmail.com",
+        {
+            streetAddress1: "Calvert Street",
+            streetAddress2: "",
+            city: " Van Nuys",
+            stateCode: "PA",
+            zipCode: '13831'
+        },
+        'This is the bio for the shelter owned by kelly.',
+        '201 344 5447',
+        "ryanhowardanimalrescue.com",
+        ["facebook.com/ryanhowardanimalrescue", "twitter.com/ryanhowardanimalrescue"],
+        ["6063d472f5a17f32a5cbdc24", "6064a5d02f56e4da24413ab3"],
+        ["6064a5f081004bcfbc33d6c5"],
+        {
+            _id: ObjectId("6063d5103833261e97e0920b"),
+            reviewDate: Date("2021-04-21T06:01:17.171Z"),
+            rating: 5,
+            reviewBody: "They were very nice and helpful when I was looking for a new pet to adopt!",
+            reviewer: ObjectId("6063d0e4be26b8a5128c59e1")
+        },
+        'http://ryanhowardanimalrescue.jpg',
+        {
+            _id: ObjectId("507f1f77bcf86cd799439011"),
+            feedback: "This website is user friendly. Posting adoptable pets through this website was easy.",
+            date: Date("2020-05-02T06:01:17.171Z"),
+            rating: 5
+        }
+
+    );
     console.log('Done seeding database');
 
     await db.serverConfig.close();
