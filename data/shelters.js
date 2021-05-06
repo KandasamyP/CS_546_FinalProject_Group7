606d8c70f8693b9e6f0eec8eb61580a3e733e59d
prashantmal1997 <prashantmall1997@gmail.com>
2021-05-06 18:28:05 -0400

    Fix server start error after merge and ran Prettier

--- data/shelters.js
+++ data/shelters.js
@@ -1,66 +1,85 @@
-const mongoCollections = require("../config/mongoCollections")
+const mongoCollections = require("../config/mongoCollections");
 let { ObjectId } = require("mongodb");
 const shelters = mongoCollections.shelters;
 
 const exportedMethods = {
-    // This async function will return the newly created shelter object
-    async addShelters(name, emailAddress, location, biography, phonenumber, website, socialmedia, availablePets, adoptedPets, reviews, websiteFeedbackGiven) {
-        // If any inputs are missing, the method should throw
-        if (!name, !emailAddress, !location, !biography, !phonenumber, !website, !socialmedia, !availablePets, !adoptedPets, !reviews, !websiteFeedbackGiven) 
-            throw "There is at least one missing input argument.";
-        
-        const sheltersCollection = await shelters();
+  // This async function will return the newly created shelter object
+  async addShelters(
+    name,
+    emailAddress,
+    location,
+    biography,
+    phonenumber,
+    website,
+    socialmedia,
+    availablePets,
+    adoptedPets,
+    reviews,
+    websiteFeedbackGiven
+  ) {
+    // If any inputs are missing, the method should throw
+    if (
+      (!name,
+      !emailAddress,
+      !location,
+      !biography,
+      !phonenumber,
+      !website,
+      !socialmedia,
+      !availablePets,
+      !adoptedPets,
+      !reviews,
+      !websiteFeedbackGiven)
+    )
+      throw "There is at least one missing input argument.";
 
-        let newShelter = {
-            name: name,
-            emailAddress: emailAddress,
-            location: location,
-            biography: biography, 
-            phonenumber: phonenumber,
-            website: website,
-            socialmedia: socialmedia,  //array
-            availablePets: availablePets, //array
-            adoptedPets: adoptedPets, // array
-            reviews: reviews,
-            websiteFeedbackGiven: websiteFeedbackGiven
-        }; 
+    const sheltersCollection = await shelters();
 
-        const insertInfo = await sheltersCollection.insertOne(newShelter);
+    let newShelter = {
+      name: name,
+      emailAddress: emailAddress,
+      location: location,
+      biography: biography,
+      phonenumber: phonenumber,
+      website: website,
+      socialmedia: socialmedia, //array
+      availablePets: availablePets, //array
+      adoptedPets: adoptedPets, // array
+      reviews: reviews,
+      websiteFeedbackGiven: websiteFeedbackGiven,
+    };
 
-        // If the shelter cannot be created, the method should throw
-        if (insertInfo.insertedCount === 0) 
-            throw "The Shelter could not be created.";
+    const insertInfo = await sheltersCollection.insertOne(newShelter);
 
-        const newId = insertInfo.insertedId;
-        let shelter = await this.getShelterById(newId.toString());
-        shelter._id = shelter._id.toString();
+    // If the shelter cannot be created, the method should throw
+    if (insertInfo.insertedCount === 0)
+      throw "The Shelter could not be created.";
 
-        return shelter;
-    },
+    const newId = insertInfo.insertedId;
+    let shelter = await this.getShelterById(newId.toString());
+    shelter._id = shelter._id.toString();
 
-    // When given an id, this function will return a shelter from the database.
-    async getShelterById(id) {
-        if (!id) 
-            throw "The input argument 'id' is missing.";
-        if (typeof id != "string") 
-            throw "The input 'id' must be a string.";
-        if (id.trim().length === 0) 
-            throw "The input 'id' must not be empty.";
+    return shelter;
+  },
 
-        let parsedId = ObjectId(id);
-        
-        const sheltersCollection = await shelters();
-        let shelter = await sheltersCollection.findOne({ _id: parsedId });
+  // When given an id, this function will return a shelter from the database.
+  async getShelterById(id) {
+    if (!id) throw "The input argument 'id' is missing.";
+    if (typeof id != "string") throw "The input 'id' must be a string.";
+    if (id.trim().length === 0) throw "The input 'id' must not be empty.";
 
-        // If the no shelter exists with that id, the method should throw
-        if (shelter === null) 
-            throw "Shelter not found";
-    
-        shelter._id = shelter._id.toString();
+    let parsedId = ObjectId(id);
 
-        return shelter;
-    }
-}
+    const sheltersCollection = await shelters();
+    let shelter = await sheltersCollection.findOne({ _id: parsedId });
 
-   
+    // If the no shelter exists with that id, the method should throw
+    if (shelter === null) throw "Shelter not found";
+
+    shelter._id = shelter._id.toString();
+
+    return shelter;
+  },
+};
+
 module.exports = exportedMethods;
