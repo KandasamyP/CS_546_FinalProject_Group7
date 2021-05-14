eadfaafb7bd8e350d8baf912f790e37558682d83
Preet Dabhi <preetdabhi14@gmail.com>
2021-05-14 00:33:52 -0400

    Running after Merge from main

--- routes/messages.js
+++ routes/messages.js
@@ -66,10 +66,16 @@ router.post("/", async (req, res) => {
         );
         isUserShelter = true;
       }
-            const thread = await messagesData.getThreadByParticipants([userInfo._id, req.body.recipient]);
-            const newMsg = await messagesData.addMessage(thread._id, userInfo._id, req.body.reply);
-            const threadList = await messagesData.getThreadsByParticipant(userInfo._id);
-            //console.log(JSON.stringify(threadList))
+
+      const thread = await messagesData.getThreadByParticipants([
+        userInfo._id,
+        req.body.recipient,
+      ]);
+      const newMsg = await messagesData.addMessage(
+        thread._id,
+        userInfo._id,
+        req.body.reply
+      );
 
       const threadList = await messagesData.getThreadsByParticipant(
         userInfo._id
